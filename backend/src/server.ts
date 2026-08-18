e0facf76d524a01eac508e2a96712d0ddb7ab307
toledorp <roge.pupo.toledo@gmail.com>
2026-08-17 22:33:02 -0300

    feat: add user management functionality

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { sequelize } from './config/database';
+import { User } from "./modesl/User";
 
 dotenv.config();
 
@@ -21,6 +22,36 @@ app.get("/api/health",  (req: Request, res: Response) => {
     });
 });
 
+//Rota Cadastro novo Usuario
+app.post ('/api/users', async (req: Request, res: Response) => {
+    try {
+        const {nome, email, senha_hash} = req.body;
+        
+        if(!nome || !email || !senha_hash) {
+            return res.status(400).json({error: 'nome, email e senha_hash são obrigatorios.'})
+        }
+        
+        const novoUsuario = await User.create({nome, email, senha_hash});
+        return res.status(201).json(novoUsuario)
+
+    } catch (error: any) {
+        return res.status(500).json({error: 'Erro ao cadastrar Usuario.', detalhe: error.message})        
+    }
+});
+
+//Rota Listar todos os usuarios
+app.get ('/api/users', async (req: Request, res:Response) => {
+    try {
+        const usuarios = await User.findAll({
+            attributes: ['id', 'nome', 'email', 'createdAt']
+        })
+        return res.status(200).json(usuarios);
+        
+    } catch (error: any) {
+        return res.status(500).json({error: 'Erro ao listar os Usuario.', detalhe: error.message})        
+    }
+})
+
 async function main() {
     try {
         await sequelize.authenticate();
@@ -32,7 +63,7 @@ async function main() {
         });
 
     } catch (error) {
-        console.log('EWrro ao conectar com o banco de dados: ', error);
+        console.log('Erro ao conectar com o banco de dados: ', error);
     }
 }
 
